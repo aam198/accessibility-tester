69c3fbd9aee88918edab874d16c2dd7ec115e448
aam198 <aam198@psu.edu>
2021-11-12 14:53:56 -0500

    Function: Fetch accessibility issues & loader

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,49 @@
-console.log('hello world')
+// Fetch accessibility issues in the backend
+
+const testAccessibility = async (e) => {
+  e.preventDefault()
+
+  //('#url').value to make sure we are getting the value from the input on submit
+  const url = document.querySelector('#url').value
+  console.log(url)
+
+  if(url === '') {
+    alert('Please add a url')
+  } else {
+    setLoading();
+
+    // Make a request and get a response with our query param.
+    const response = await fetch (`/api/test?url=${url}`)
+
+    // Checking response status
+    if(response.status !== 200){
+      setLoading(false)
+      alert('Oh snap! Something went wrong')
+    }
+    else {
+      const {issues} = await response.json()
+      console.log(issues);
+    }
+  }
+}
+
+// Add Issues to the DOM
+
+
+// Set the loading state 
+
+const setLoading = (isLoading = true) => {
+  const loader = document.querySelector('.loader');
+  if(isLoading){
+    loader.style.display = 'block';
+  }
+  else{
+    loader.style.display = 'none';
+  }
+
+}
+// Escape HTML
+
+
+
+document.querySelector('#form').addEventListener('submit', testAccessibility)
